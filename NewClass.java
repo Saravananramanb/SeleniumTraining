d35b118587a8f2ad6093acf539c74c2491dfd726
Saravananramanb <85895152+Saravananramanb@users.noreply.github.com>
2021-06-14 23:59:38 +0530

    Add files via upload

---
+++ NewClass.java
@@ -0,0 +1,16 @@
+package Day1NewPackage;
+
+import week1.day1.Mobile;
+
+public class NewClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Mobile newObj = new Mobile();
+		newObj.sendSms();
+		newObj.makeCall();
+
+	}
+
+}
